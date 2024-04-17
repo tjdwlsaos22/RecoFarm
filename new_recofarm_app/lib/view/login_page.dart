b5f8487247a47d2a33a279c387e45aee52bcb707
pulpilisory <pulpilisory@gmailcom>
2024-04-17 14:29:56 +0900

    2024.04.17 pdg : android 설정, ios gps , image 설정 업데이트 , login page ui

---
+++ new_recofarm_app/lib/view/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+/*
+ * Description : Login Page 
+ * Date : 2024.04.17
+ * Author : Forrest Parkm (pdg)
+ * Update : 
+ *  - 2024.04.17 by pdg
+ *    1.  login page UI 
+ * 
+ */
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- new_recofarm_app/lib/view/mainview.dart
+++ new_recofarm_app/lib/view/mainview.dart
@@ -4,9 +4,12 @@ import 'package:new_recofarm_app/view/home_view_page.dart';
 
 /*
  
-  Description : 현재 Page는 추후에 로그인 이후 가장먼저 사용자에게 보여주는 홈화면으로 만들 생각입니다.
+  Description : 
+          
+                현재 Page는 추후에 로그인 이후 가장먼저 사용자에게 보여주는 홈화면으로 만들 생각입니다.
                 TabBarView를 이용하여 각각 tabbar에 맞는 class를 만들어 주시면 됩니다.
                 tabbar를 몇개로 할건지 또한 미정. 
+
   Date        : 2024-04-17 13:32
   Author      : lcy
   Updates     : 
